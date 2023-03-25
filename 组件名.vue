50b56e14fd00873039f578ac577e573347f2f03f
Ohuo_l <2114845946@qq.com>
2023-03-25 17:13:53 +0800

    分页器组件

---
+++ 组件名.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <!-- 分页 -->
+        <Pagination
+          :pageNo="page"
+          :pageSize="limit"
+          :total="myOrder.total"
+          :continues="5"
+          @getPageNo="getPageNo"
+        ></Pagination>
+    </div>
+  </template>
+  <script>
+  export default {
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
